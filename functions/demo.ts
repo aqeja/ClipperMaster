f0bdd999dc68899a79bef776c6f565eae95bede5
jiakun <aqeja@outlook.com>
2024-06-03 15:50:15 +0800

    functions

--- functions/demo.ts
+++ functions/demo.ts
@@ -1,7 +1,5 @@
 export async function onRequest(context) {
-  // Create a prepared statement with our query
-  const ps = context.env.NORTHWIND_DB.prepare("SELECT * from users");
-  const data = await ps.first();
-
-  return Response.json(data);
+  return Response.json({
+    name: "Tony",
+  });
 }
